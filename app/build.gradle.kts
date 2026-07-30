50541ae823d0171545c7d733ee698d0187c371eb
brutus1214 <brutus1214@gmail.com>
2026-07-30 10:10:08 -0400

    Bump app version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jameschang.jcmetatranslatehelper"
         minSdk = 31
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.3.0"
     }
 
     buildFeatures {
